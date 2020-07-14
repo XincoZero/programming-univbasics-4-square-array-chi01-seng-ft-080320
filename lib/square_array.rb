a5f993685a40c0bebbd6b5b9ca62f5fa01232e7b
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-14 00:47:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
-  new_array = []
-  array = array.each{|x| new.push(x**2)}
-  new_array
+  squared_array = []
+  count = 0 
+  while count < array.size do 
+    squared_array << (array[count]**2)
+    count = count += 1
+  end 
+  squared_array
 end
