b4a6a9dd957c0da6a0d217a317a8e7407f560a7b
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-14 00:23:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   numbers = [1, 2, 3]
   squares_array = []
-  while numbers.each do |number|
+  numbers.each do |number|
     squares_array << number*number
   end
   squares_array
